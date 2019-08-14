80c18e419bf8691b106fac198849cc23dc4ce847
Anton Shein <adrior@yandex-team.ru>
2019-08-14 07:07:19 +0300

    Unique Username Index

---
+++ migrations/20190814064153_unique.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.alterTable("user", function(t) {
+    t.unique("username");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("user", function(t) {
+    t.dropUnique("username");
+  });
+};
